2b165cab4f51f71728090b223b18005453af7cb5
manmuba <manmuba90@gmail.com>
2023-06-09 09:01:07 +0300

    models

--- backend/competitions/models.py
+++ backend/competitions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Competitions(models.Model):
+    image       = models.ImageField(upload_to="competitions")
+    name        = models.CharField(max_length=150)
+    description = models.TextField()
+
+    def short_description(self):
+        return self.description[:50]
+    
+    def __str__(self):
+        return self.name
