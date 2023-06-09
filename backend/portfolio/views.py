e81ce0f76d0da11840df6869b5f3dbdf73ef4815
manmuba <manmuba90@gmail.com>
2023-06-09 10:06:40 +0300

    update jinja template

--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -5,8 +5,12 @@ from projects.models import Category, Project
 from service.models import Service
 from skill.models import Skill
 from testimonial.models import Testimonials
+from tutorial.models import Tutorial
 
 def home(request):
+    tutorial = Tutorial.objects.all()
+    tutorial1 = tutorial[:len(tutorial)//2]
+    tutorial2 = tutorial[len(tutorial)//2:]
     blog = Blog.objects.all()
     category = Category.objects.all()
     education = Education.objects.all()
@@ -27,5 +31,7 @@ def home(request):
         'education': education,
         'work': work,
         "testimonials": testimonials,
+        'tutorial1': tutorial1,
+        'tutorial2': tutorial2,
     }
     return render(request, 'index.html', context)
