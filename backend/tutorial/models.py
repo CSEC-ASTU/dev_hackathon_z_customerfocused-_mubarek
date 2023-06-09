027d4f31a60dfcb9b517ea5b15592c1769db5627
manmuba <manmuba90@gmail.com>
2023-06-09 09:42:04 +0300

    tutorial app

--- backend/tutorial/models.py
+++ backend/tutorial/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class Competitions(models.Model):
-    image       = models.ImageField(upload_to="competitions")
+class Tutorial(models.Model):
+    image       = models.ImageField(upload_to="tutorial")
     name        = models.CharField(max_length=150)
-    year        = models.IntegerField()
+    link        = models.CharField(max_length=500)
     description = models.TextField()
 
     def short_description(self):
-        return self.description[:50]
+        return self.description[:80]
     
     def __str__(self):
         return self.name
