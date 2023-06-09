8c58bc2daf72e25424f4966f6d5f8b6a647552f7
manmuba <manmuba90@gmail.com>
2023-06-09 09:44:55 +0300

    editting template

--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
+from blog.models import Blog
+from experience.models import Work, Education
+from projects.models import Category, Project
+from service.models import Service
+from skill.models import Skill
+from testimonial.models import Testimonials
 
 def home(request):
+    blog = Blog.objects.all()
+    category = Category.objects.all()
+    education = Education.objects.all()
+    projects = Project.objects.all()
+    services = Service.objects.all()
+    skill = Skill.objects.all()
+    skill1 = skill[:len(skill)//2]
+    skill2 = skill[len(skill)//2::]
+    work = Work.objects.all()
+    testimonials = Testimonials.objects.all()
     context = {
-        
+        "blog": blog,
+        'category': category,
+        'projects': projects,
+        'skill1': skill1,
+        'skill2': skill2,
+        'services': services,
+        'education': education,
+        'work': work,
+        "testimonials": testimonials,
     }
-    return render(request, "index.html", context)
+    return render(request, 'index.html', context)
