302091905fd93450b50d2c24d2dc94bc558c6113
Mubarek Shikur <manmuba90@gmail.com>
2023-06-11 08:39:27 +0300

    Update settings.py secret key

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-67_1%p568xbr^het=e&_*ep3bvcu^^0z@1_0+lx6_1!d^(*1z!'
+SECRET_KEY = 'Your Secret Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
